9c10c925cbf4d68fca6e41c2ab1ed88fb5b3108c
afariya <tilikiptiken@gmail.com>
2024-08-28 18:19:57 +0300

    added home page - chat support

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 const chatToggle = document.getElementById('chatToggle');
 const chatContainer = document.getElementById('chatContainer');
+const closeChat = document.querySelector('.close-chat');
 
 chatToggle.addEventListener('click', function () {
     if (chatContainer.style.display === 'none' || chatContainer.style.display === '') {
@@ -9,4 +10,9 @@ chatToggle.addEventListener('click', function () {
         chatContainer.style.display = 'none';
         chatToggle.style.display = 'block';
     }
+});
+
+closeChat.addEventListener('click', function () {
+    chatContainer.style.display = 'none';
+    chatToggle.style.display = 'flex';
 });
